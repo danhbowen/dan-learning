8fbf2e2888c1efe9cc6aa449785e3b28db7d4556
Dan Hameiri-Bowen <dan@Dans-MacBook-Pro.local>
2017-11-13 21:45:26 +0000

    what git commands do 1.0

--- test1.py
+++ test1.py
@@ -1,1 +1,7 @@
 # test script
+# Git status -
+# Git add . = commit everything
+# git status (see what has changed)
+# Git commit -m "Message for me to read later - IMPORTANT"
+#Git push (moves it into the git browser)
+
